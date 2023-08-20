209f24ed8f9fa693297a398c700ac658d44b7bc1
leonardo <leobessi03@gmail.com>
2023-08-20 13:05:36 +0200

    Better cmake layout

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QPushButton>
 #include "server/ChatServer.h"
 #include "client_api/ChatAPI.h"
-#include "model/ChatRegister.h"
+#include "ChatRegister.h"
 #include "gui/ChatRegisterController.h"
 #include "gui/chatregisterview.h"
 #include "client/ChatApp.h"
